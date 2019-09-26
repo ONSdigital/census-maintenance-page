cc758d99b24af514df2569c2bfdf2f025652f347
Andrew Maddaford <ajmaddaford@gmail.com>
2019-09-26 09:53:33 +0100

    Update port and healthcheck endpoint to work with eQ
    The eQ version needs to run on the same port and have the same
    healthcheck as eQ because it will be deployed in place of runner.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t eu.gcr.io/census-gcr-int/census-maintenance-page:latest .
-docker push eu.gcr.io/census-gcr-int/census-maintenance-page:latest
+docker build -t eu.gcr.io/census-eq-ci/eq-census-maintenance-page:latest .
+docker push eu.gcr.io/census-eq-ci/eq-census-maintenance-page:latest
