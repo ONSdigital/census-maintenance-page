49ca4618f5c1f9c243c4182f9f7e9e5c89fbd847
Rob Smart <rob@starcloud.solutions>
2019-10-11 16:38:21 +0100

    create distinct docker files for eq and website(rh) images

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-docker build -t eu.gcr.io/census-gcr-eq/eq-census-maintenance-page:latest .
+docker build -f Dockerfile-eq -t eu.gcr.io/census-gcr-eq/eq-census-maintenance-page:latest .
 docker push eu.gcr.io/census-gcr-eq/eq-census-maintenance-page:latest
 
-docker build -t eu.gcr.io/census-gcr-int/census-maintenance-page:latest .
+docker build -f Dockerfile-web -t eu.gcr.io/census-gcr-int/census-maintenance-page:latest .
 docker push eu.gcr.io/census-gcr-int/census-maintenance-page:latest
