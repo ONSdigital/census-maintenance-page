1e21de332e8658d2d990bf06e67d75758903c3fa
Rob Smart <rob@starcloud.solutions>
2019-10-11 16:22:28 +0100

    also build rh maintenance page docker image

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 docker build -t eu.gcr.io/census-gcr-eq/eq-census-maintenance-page:latest .
 docker push eu.gcr.io/census-gcr-eq/eq-census-maintenance-page:latest
+
+docker build -t eu.gcr.io/census-gcr-int/census-maintenance-page:latest .
+docker push eu.gcr.io/census-gcr-int/census-maintenance-page:latest
